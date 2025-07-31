2893c5404b76e4d04f7737e26c4e81a82c2c4d58
Reizal <122618833+ReizalPambuko@users.noreply.github.com>
2025-07-31 17:25:11 +0700

    porto

--- src/component/header/HeaderSocial.jsx
+++ src/component/header/HeaderSocial.jsx
@@ -7,28 +7,7 @@ import { SiGmail } from "react-icons/si";
 
 const HeaderSocial = () => {
   return (
-    <div className="container__social">
-      <a href="https://www.linkedin.com/in/mohammad-reizal-a789b7244/">
-        {" "}
-        <BsLinkedin className="container__social-icon" />
-      </a>
-      <a href="https://bit.ly/3he4dBE">
-        {" "}
-        <IoLogoWhatsapp className="container__social-icon" />
-      </a>
-      <a href="https://www.youtube.com/channel/UCKKd7Ik8mFcM6q7YkgaeG6w">
-        {" "}
-        <BsYoutube className="container__social-icon" />
-      </a>
-      <a href="https://github.com/ReizalPambuko">
-        {" "}
-        <BsGithub className="container__social-icon" />
-      </a>
-      <a href="mailto:mohammadreizall@gmail.com">
-        {" "}
-        <SiGmail className=" container__social-icon" />
-      </a>
-    </div>
+   <h4>...</h4>
   );
 };
 
